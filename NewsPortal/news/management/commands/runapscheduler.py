c8b33e8161aaa93a92e66545b5d7e64fce5cb270
b1gmouse <80082626+b1gmouse@users.noreply.github.com>
2023-03-27 00:17:23 +1000

    main

--- NewsPortal/news/management/commands/runapscheduler.py
+++ NewsPortal/news/management/commands/runapscheduler.py
@@ -3,21 +3,51 @@ import logging
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
 from django.conf import settings
-from django.core.mail import mail_managers
+from django.core.mail import mail_managers, EmailMultiAlternatives
 from django.core.management.base import BaseCommand
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
+from django.template.loader import render_to_string
 from django_apscheduler import util
 from django_apscheduler.jobstores import DjangoJobStore
 from django_apscheduler.models import DjangoJobExecution
+from news.tasks import new_post_subscription
 
-from news.models import Post
+from news.models import Post, PostCategory
 
 logger = logging.getLogger(__name__)
 
 
-def my_job():
-    news = Post.objects.order_by('topic')[:3]
-    text = '\n'.join(['{}'.format(p.topic) for p in news])
-    mail_managers("Самые свежие новости", text)
+def get_subscriber(category):
+    user_email = []
+    for user in category.subscribers.all():
+        user_email.append(user.email)
+    return user_email
+
+
+def new_post_subscription(instance):
+    template = 'new_post.html'
+
+    for category in instance.category.all():
+        email_subject = f'New post in category: "{category}"'
+        user_emails = get_subscriber(category)
+
+        html = render_to_string(
+            template_name=template,
+            context={
+                'category': category,
+                'post': instance,
+            },
+        )
+        msg = EmailMultiAlternatives(
+            subject=email_subject,
+            body='',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            to=user_emails,
+        )
+
+    msg.attach_alternative(html, 'text/html')
+    msg.send()
 
 
 @util.close_old_connections
@@ -31,20 +61,24 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         scheduler = BlockingScheduler(timezone=settings.TIME_ZONE)
         scheduler.add_jobstore(DjangoJobStore(), "default")
-
-        scheduler.add_job(
-            my_job,
-            trigger=CronTrigger(minute="20", hour="10"),
-            id="my_job",  # The `id` assigned to each job MUST be unique
-            max_instances=1,
-            replace_existing=True,
-        )
+        @receiver(m2m_changed, sender=PostCategory)
+        def notify_subscribers(sender, instance, **kwargs):
+            scheduler.add_job(
+                new_post_subscription(instance),
+                trigger=CronTrigger(second="*/5"),
+              #trigger=CronTrigger(
+                #day_of_week="fri", hour="18", minute="00"
+             #),
+                id="my_job",  # The `id` assigned to each job MUST be unique
+                max_instances=1,
+                replace_existing=True,
+            )
         logger.info("Added job 'my_job'.")
 
         scheduler.add_job(
             delete_old_job_executions,
             trigger=CronTrigger(
-                day_of_week="mon", hour="00", minute="00"
+                day_of_week="fri", hour="18", minute="00"
             ),
             id="delete_old_job_executions",
             max_instances=1,
